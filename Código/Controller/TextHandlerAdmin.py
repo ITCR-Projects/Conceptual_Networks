a05d035a94c6ba2c37f709dd314a363c9c0f8407
Josue BS <jbarrientossandoval@estudiantec.cr>
2023-08-26 02:01:04 -0600

    - Se agrega la implemetación del objeto WebFile - Se agrego el código para que el WebFile funcione en textHandler

--- Código/Controller/TextHandlerAdmin.py
+++ Código/Controller/TextHandlerAdmin.py
@@ -1,6 +1,7 @@
 import os
 
 from Código.Model.DocxFile import DocxFile
+from Código.Model.WebFile import WebFile
 
 
 class TextHandlerAdmin:
@@ -11,8 +12,13 @@ class TextHandlerAdmin:
     def add_files(self, files):
         for filepath in files:
             name, extension = os.path.splitext(filepath)
+
             if extension == ".docx":
                 file = DocxFile(name, filepath)
+                self.files.append(file)
+                print(file.get_text())
+            elif extension == "":
+                file = WebFile(name, filepath)
                 self.files.append(file)
                 print(file.get_text())
 
--- Código/View/main.py
+++ Código/View/main.py
@@ -2,4 +2,5 @@ from Código.Controller.Controller import MainController
 
 if __name__ == '__main__':
     main_controller = MainController()
-    main_controller.addFiles(['prueba.docx'])
+    main_controller.addFiles(['https://www.recetasderechupete.com/pollo-asado-facil-al-horno-con-patatas-panaderas/9535/', "https://es.wikipedia.org/wiki/Muffin#:~:text=Un%20muffin%2C%20conocido%20en%20espa%C3%B1ol,ancha%2C%20con%20forma%20de%20hongo"])
+    # main_controller.addFiles(['prueba.docx'])
