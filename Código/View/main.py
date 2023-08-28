7ee5ec9ab453921943aaf83fa68c63adede6f3b8
MrManito <gandresgv16@estudiantec.cr>
2023-08-28 14:41:44 -0600

    Ventana principal

--- Código/View/main.py
+++ Código/View/main.py
@@ -1,5 +1,78 @@
+# PyQt6 dependencies
+from PyQt6.QtWidgets import QApplication, QToolBar, QMainWindow, QGridLayout, QListWidget, QFileDialog, QPushButton, QLineEdit, QWidget
+# Import sys to work with system operations
+import sys
+# Controller class
 from Código.Controller.Controller import MainController
 
-if __name__ == '__main__':
-    main_controller = MainController()
-    main_controller.addFiles(['prueba.docx'])
+# Main window class
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.main_controller = MainController()
+        # Define the layout of the window
+        self.mwlayout = QGridLayout()
+        self.mwlayout.setSpacing(10)
+
+        # Add a toolbar on the top of the window
+        toolbar = QToolBar("My main toolbar")
+        self.addToolBar(toolbar)
+
+        # The list widget
+        self.file_list = QListWidget()
+        self.mwlayout.addWidget(self.file_list, 0, 0, 1, 1)
+
+        # Text box to contain the URL
+        self.url_txb = QLineEdit()
+        self.mwlayout.addWidget(self.url_txb, 0, 2)
+
+        # Button to add the URL to the file list
+        add_url_btn = QPushButton("Add URL")
+        add_url_btn.clicked.connect(self.add_url_to_list)
+        self.mwlayout.addWidget(add_url_btn,1, 2)
+
+        # Add a button to add files
+        add_files_frame_btn = QPushButton("Add Files")
+        add_files_frame_btn.clicked.connect(self.add_files)
+        self.mwlayout.addWidget(add_files_frame_btn, 2, 2)
+
+        # Add a button to start the creation of the graph
+        create_graph_btn = QPushButton("Process Files")
+        create_graph_btn.clicked.connect(self.process_files)
+        self.mwlayout.addWidget(create_graph_btn, 2,0)
+
+        widget = QWidget()
+        widget.setLayout(self.mwlayout)
+        self.setCentralWidget(widget)
+
+    # Function that adds the files to the list
+    def add_files(self):
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
+        file_dialog.setNameFilter("All Files (*.*)")
+        if file_dialog.exec() == QFileDialog.DialogCode.Accepted:
+            selected_files = file_dialog.selectedFiles()
+            self.file_list.addItems(selected_files)
+
+    # Function that adds the URLs to the list
+    def add_url_to_list(self):
+        text = self.url_txb.text()
+        if text:
+            self.file_list.addItem(text)
+            self.url_txb.clear()
+
+    # Function that process the selected files and URL
+    def process_files(self):
+        items = []
+        for i in range(self.file_list.count()):
+            item = self.file_list.item(i)
+            items.append(item.text())
+        print(f"Items {items}")
+        self.main_controller.addFiles(items)
+
+
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+app.exec()
+
