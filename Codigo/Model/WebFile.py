fc3bc650179d718ac2a5742ae0072be50a993adb
Josue BS <jbarrientossandoval@estudiantec.cr>
2023-08-29 15:03:44 -0600

    Exceptions in class WebFile.py

--- Codigo/Model/WebFile.py
+++ Codigo/Model/WebFile.py
@@ -9,15 +9,37 @@ class WebFile(File, ABC):
         super().__init__(name, url_file)
 
     def get_text(self):
-        # Se obtiene la pagina web
-        response = requests.get(self.name)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        try:
+            # Se obtiene la pagina web
+            response = requests.get(self.name)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        name_txt = soup.title.string.replace(' ', '_')
-        self.url_file = name_txt + ".txt"
-        text = soup.body.get_text()
+            name_txt = soup.title.string.replace(' ', '_')
+            self.url_file = name_txt + ".txt"
+            text = soup.body.get_text()
 
-        # Se crea el archivo .txt y se guarda con la infoamción de la página
-        with open(self.path + self.url_file, "w", encoding="utf-8") as f:
-            f.write(text)
-        return text
+            # Se crea el archivo .txt y se guarda con la infoamción de la página
+            with open(self.path + self.url_file, "w", encoding="utf-8") as f:
+                f.write(text)
+
+            return {
+                'response': False,
+                'message': text,
+
+            }
+        except FileNotFoundError as e:
+            return {
+                'response': True,
+                'message': f"Error: El archivo {self.url_file} no se encontró.",
+            }
+        except requests.exceptions.RequestException as e:
+            return {
+                'response': True,
+                'message': f"{e}",
+            }
+        except Exception as e:
+            return {
+                'response': True,
+                'message': "Error: No se logró cargar el archivo.",
+            }
